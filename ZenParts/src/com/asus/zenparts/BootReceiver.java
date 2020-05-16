5b1cc6a1052ef52224dc83e736d52b08a99dfcba
NurKeinNeid <mralexman3000@gmail.com>
2020-05-16 12:29:14 +0530

    X01AD: ZenParts: Implemented FPS info in zenparts
    ->Add sepolicy rules
    ->Add new drawable picked from https://materialdesignicons.com/
    - Original Commit
    DerpFest-Devices/device_oneplus_msm8998-common@e6ec51c#diff-195d2c91fafb0c372a1d6dc13d2bba4e
    DerpFest-Devices/device_oneplus_msm8998-common@2eb39fa#diff-195d2c91fafb0c372a1d6dc13d2bba4e
    
    Signed-off-by: Prabhat774 <mprabhat774@gmail.com>

--- ZenParts/src/com/asus/zenparts/BootReceiver.java
+++ ZenParts/src/com/asus/zenparts/BootReceiver.java
@@ -19,6 +19,8 @@ package com.asus.zenparts;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
 import android.provider.Settings;
 import com.asus.zenparts.preferences.VibratorStrengthPreference;
 
@@ -36,6 +38,8 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
     private final String MICROPHONE_GAIN_PATH = "/sys/kernel/sound_control/mic_gain";
 
     public void onReceive(Context context, Intent intent) {
+    
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         FileUtils.setValue(TORCH_1_BRIGHTNESS_PATH,
                 Settings.Secure.getInt(context.getContentResolver(),
@@ -76,6 +80,12 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
             FileUtils.setValue(KCAL_HUE, Settings.Secure.getInt(context.getContentResolver(),
                     PREF_HUE, HUE_DEFAULT));
         VibratorStrengthPreference.restore(context);
+        
+        //FPS
+        boolean enabled = sharedPrefs.getBoolean(DeviceSettings.PREF_KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+
         }
     }
 }
