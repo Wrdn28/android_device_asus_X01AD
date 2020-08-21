1cb5c40928a423cda6e24455ec5412105d48256c
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    X01AD: rro: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -402,6 +402,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
