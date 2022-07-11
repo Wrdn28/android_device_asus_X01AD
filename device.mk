83e56d96954164fe4f17ba36efeaeda49cf380f0
Dhina17 <dhinalogu@gmail.com>
2022-07-11 14:44:49 +0530

    X01AD: Update Radio/IMS blobs from FP3
    Change-Id: I0e4d713a03a25bd26d50b52ccb5e48d042b73986
    
     Author:    Dhina17 <dhinalogu@gmail.com>
     Date:      Mon Jul 11 14:44:49 2022 +0530

--- device.mk
+++ device.mk
@@ -371,7 +371,8 @@ PRODUCT_COPY_FILES += \
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/permissions/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext_privapp-permissions-qti.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
