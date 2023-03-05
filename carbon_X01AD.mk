0ee87733914c2b4978f6d646288bf26bed5815de
Yara0202 <doodle280204@gmail.com>
2023-03-05 10:06:16 +0700

    X01AD: Adapt tree

--- carbon_X01AD.mk
+++ carbon_X01AD.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+# Inherit some common CarbonROM stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
