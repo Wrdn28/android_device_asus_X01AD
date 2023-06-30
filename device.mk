efec6c910f287a07cd1018a133c60ebea8619fb8
rayrzy <vincentrayananda@gmail.com>
2023-06-30 05:40:58 +0000

    X01AD: Import Prebuilt Google Camera Go
    * Google Camera Go Special Build 2 by Greatness
    
    https://www.celsoazevedo.com/files/android/google-camera/dev-greatness/f/dl9/
    
    Changelog:
    - This is an incremental update to my last build
    - Activated Option to Change Aspect Ratio
    - HDR improved Dramatically, Much more natural colors now and Highlights are no more overblown
    - Like last time, I am not responsible for any application other than the one with below hash. So be careful.
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I0452f58832b81ee41fc6c3e6359335d6c6d1e0cc
    Signed-off-by: Hafizh Arrizqa <hafizharrizqa15@gmail.com>
    Signed-off-by: Rahmad Adi Pratama <OhMyVenyx@proton.me>

--- device.mk
+++ device.mk
@@ -226,7 +226,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
-# Health
+# Prebuilt GoogleCameraGo
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
+# Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery \
