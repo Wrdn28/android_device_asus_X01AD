40ed4f4821ac03d70ed528e3ef5fab2869164388
Wrdn28 <ifloraxd@gmail.com>
2023-07-16 12:16:18 +0000

    Revert "X01AD: power: Create dummy libqti-perfd-client"
    This reverts commit eacc8adc73dc0ea00dac4d834ae4aed73eea7a46.

--- device.mk
+++ device.mk
@@ -346,14 +346,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor \
-    libqti-perfd-client
-
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
