ecd7a399f09264619530838626aac460ccd46e52
Wrdn28 <ifloraxd@gmail.com>
2023-07-26 04:52:14 +0000

    Revert "X01AD: Migrate to AIDL ClearKey DRM HAL"
    This reverts commit d909a091d06c64e0b0ce077ca0941e3f587d1968.

--- device.mk
+++ device.mk
@@ -205,7 +205,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
