21e7d6e3db5c2836de6ba8a1ba2aca312a7ed000
Wrdn28 <ifloraxd@gmail.com>
2023-08-04 16:51:32 +0000

    X01AD: Switch to XDroid Dark Bootanimation

--- xdroid_X01AD.mk
+++ xdroid_X01AD.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/asus/X01AD/device.mk)
 # Inherit some common xdroid stuff.
 $(call inherit-product, vendor/xdroid/config/common.mk)
 
+# xdroid Flags
+XDROID_BOOT_DARK := true
+
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
