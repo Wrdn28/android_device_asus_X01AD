cbac46dab1119d66bfbb709dd5fc5530541aded3
Wrdn28 <ifloraxd@gmail.com>
2023-08-21 03:07:51 +0700

    X01AD: Removes junk files

--- Android.mk
+++ Android.mk
@@ -1,9 +1,5 @@
 #
-<<<<<<< HEAD
-# Copyright (C) 2020-21 The Android Open-Source Project
-=======
 # Copyright (C) 2017, 2022 The LineageOS Project
->>>>>>> 381a30f1 (onclite: Update Radio/IMS blobs from FP3)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
