a676624e6c9e68f9187c882ccb7f5197a903a035
Wrdn28 <ifloraxd@gmail.com>
2023-08-28 01:21:43 +0700

    X01AD: set default screen density

--- device.mk
+++ device.mk
@@ -54,6 +54,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
