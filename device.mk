d559d9a09c80fe29b50253f4ec3e74fbd1777278
Bruno Martins <bgcngm@gmail.com>
2023-11-29 17:54:24 +0800

    X01AD: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -192,7 +192,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
